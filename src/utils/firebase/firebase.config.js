a5be5cd8d612fc83c20e36d4d1c0f0b5d2f22048
masudrahman-dev <masudrahman.dev@gmail>
2023-05-01 22:33:25 +0600

    deploy complete on firebase

--- src/utils/firebase/firebase.config.js
+++ src/utils/firebase/firebase.config.js
@@ -4,14 +4,13 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// console.log(import.meta.env.vite_apikey);
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
+  apiKey: "AIzaSyBj6XX8M4HvpWVAmPBFy3wijXfGEhE_WWM",
+  authDomain: "dragon-news-client-3fe68.firebaseapp.com",
+  projectId: "dragon-news-client-3fe68",
+  storageBucket: "dragon-news-client-3fe68.appspot.com",
+  messagingSenderId: "204706857496",
+  appId: "1:204706857496:web:bc309c8d36649084d3796d",
 };
 
 // Initialize Firebase
